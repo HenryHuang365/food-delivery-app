d064ab63407ab428679a0cee2833de01f4acca19
HenryHuang365 <jinghuang365@gmail.com>
2025-01-01 14:03:32 +1100

    cart items slice on cart icon

--- components/cartIcon.tsx
+++ components/cartIcon.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { themeColors } from '@/theme';
 import { useRouter } from 'expo-router';
 import { useSelector } from 'react-redux';
-import { selectCartItems } from '@/slices/cart-slice';
+import { selectCartItems, selectCartTotal } from '@/slices/cart-slice';
 
 export default function CartIcon() {
   const router = useRouter();
   const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
   if (cartItems.length === 0) return; // If no items are added, do not show cart icon.
 
   return (
@@ -21,12 +22,14 @@ export default function CartIcon() {
           className="p-2 px-4 rounded-full"
           style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)' }}
         >
-          <Text className="font-extrabold text-white text-lg">3</Text>
+          <Text className="font-extrabold text-white text-lg">
+            {cartItems.length}
+          </Text>
         </View>
         <Text className="flex-1 text-center font-extrabold text-white text-lg">
           View Cart
         </Text>
-        <Text className="font-extrabold text-white text-lg">${23}</Text>
+        <Text className="font-extrabold text-white text-lg">${cartTotal}</Text>
       </TouchableOpacity>
     </View>
   );
